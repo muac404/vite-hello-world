77be10dda278c63b256a6bad9aeb66a11ece2b32
muac404 <muac404@gmail.com>
2022-11-23 16:14:53 +0100

    Aggiunge organizzazione file e component "AppTitle"

---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitle from "@/components/AppTitle.vue";
+export default {
+    components: {
+        AppTitle
+    }
+}
+    // Codice javascript
+
+</script>
+
+<template>
+    <AppTitle />
+
+    <!-- html -->
+
+</template>
+
+<style scoped>
+/* Css */
+</style>
